eb974ae4260dcd01fb6fe29926264751676615f4
fazil roshan <2k21cse027@kiot.ac.in>
2025-01-29 10:36:58 +0530

    Initial Commit

---
+++ demogitproject2/src/demogitproject2/Sample2.java
@@ -0,0 +1,7 @@
+package demogitproject2;
+
+public class Sample2 {
+	public static void main(String[] args) {
+		System.out.println("bye");
+	}
+}
---
+++ demogitproject2/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module demogitproject2 {
+}
